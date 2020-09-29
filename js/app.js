b3632929277edc85b90454c64b2833b600d9f4cf
Tony <tony.panczenko@gmail.com>
2020-09-29 13:35:31 +0100

    add html content add css styles add baguettebox lightbox js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// jscs:disable validateLineBreaks
+/*jshint esversion: 6 */
+
+const baguetteBox = require('baguettebox.js');
+
+window.addEventListener('load', function () {
+  baguetteBox.run('.gallery-container');
+});
